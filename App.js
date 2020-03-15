9fcafddcec401e0d14d4a18f3df42333ea33936f
hgee <u_hgee@naver.com>
2020-03-15 16:26:18 +0900

    너무재밌어요

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>우와개신기해</Text>
+      <Button title="CLICK" onPress={() => Alert.alert('혜지공주 짱')} />
     </View>
   );
 }
