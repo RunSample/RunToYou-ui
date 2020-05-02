be5b804d23eaa19068e73c917b1b76a2d7b262c0
tommy8808 <tommy8808@gmail.com>
2020-05-02 22:41:37 +0900

    구글 맵 추가

--- App.js
+++ App.js
@@ -19,7 +19,7 @@ export default function App() {
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setIsLoding(false); // count is 0 here
+      setIsLoding(false);
     }, 2000);
     return () => clearTimeout(timer);
   }, []);
